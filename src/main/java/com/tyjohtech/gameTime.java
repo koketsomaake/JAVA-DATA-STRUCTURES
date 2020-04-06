9180d9644e7072e7651e7f31d0629f4e2e5d51cd
koketsomaake <koketsomaake95@gmail.com>
2020-04-06 16:42:08 +0200

    initial commit

---
+++ src/main/java/com/tyjohtech/gameTime.java
@@ -0,0 +1,67 @@
+package com.tyjohtech;
+
+public class gameTime {
+
+    int width;
+    int height;
+    int[][] board;
+
+    public gameTime(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.board = new int[width][height];
+    }
+
+    public void printBoard() {
+        System.out.println("---");
+        for (int y = 0; y < height; y++) {
+            String line = "|";
+            for (int x = 0; x < width; x++) {
+                if (this.board[x][y] == 0) {
+                    line += ".";
+                } else {
+                    line += "*";
+                }
+            }
+            line += "|";
+            System.out.println(line);
+        }
+        System.out.println("---\n");
+    }
+
+    public void setAlive(int x, int y) {
+        this.board[x][y] = 1;
+    }
+
+    public void setDead(int x, int y) {
+        this.board[x][y] = 0;
+    }
+
+    public int countAliveNeighbours(int x, int y) {
+
+    }
+
+    public int getState(int x, int y) {
+        if (x < 0 || x >= width) {
+            return 0;
+        }
+
+        if (y < 0 || y >= height) {
+            return 0;
+        }
+
+        return this.board[x][y];
+    }
+
+    public void step() {
+
+    }
+
+    public static void main(String[] args) {
+        gameTime simulation = new gameTime(10, 10);
+
+
+
+    }
+
+}
