e4768d34db0d270db2ad6aceceb6ca296a47db43
koketsomaake <59966150+koketsomaake@users.noreply.github.com>
2020-04-08 10:03:45 +0200

    Update gameTime.java

--- src/main/java/com/tyjohtech/gameTime.java
+++ src/main/java/com/tyjohtech/gameTime.java
@@ -18,9 +18,9 @@ public class gameTime {
             String line = "|";
             for (int x = 0; x < width; x++) {
                 if (this.board[x][y] == 0) {
-                    line += ".";
+                    line += " +";
                 } else {
-                    line += "*";
+                    line += " #";
                 }
             }
             line += "|";
@@ -37,8 +37,21 @@ public class gameTime {
         this.board[x][y] = 0;
     }
 
-    public int countAliveNeighbours(int x, int y) {
+    public int countAlive(int x, int y) {
+        int count = 0;
 
+        count += getState(x - 1, y - 1);
+        count += getState(x, y - 1);
+        count += getState(x + 1, y - 1);
+
+        count += getState(x - 1, y);
+        count += getState(x + 1, y);
+
+        count += getState(x - 1, y + 1);
+        count += getState(x, y + 1);
+        count += getState(x + 1, y + 1);
+
+        return count;
     }
 
     public int getState(int x, int y) {
@@ -54,13 +67,48 @@ public class gameTime {
     }
 
     public void step() {
+        int[][] newBoard = new int[width][height];
 
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int aliveBox = countAlive(x, y);
+
+                if (getState(x, y) == 1) {
+                    if (aliveBox < 2) {
+                        newBoard[x][y] = 0;
+                    } else if (aliveBox == 2 || aliveBox == 3) {
+                        newBoard[x][y] = 1;
+                    } else if (aliveBox > 3) {
+                        newBoard[x][y] = 0;
+                    }
+                } else {
+                    if (aliveBox == 3) {
+                        newBoard[x][y] = 1;
+                    }
+                }
+
+            }
+        }
+
+        this.board = newBoard;
     }
 
     public static void main(String[] args) {
         gameTime simulation = new gameTime(10, 10);
 
+        simulation.setAlive(2, 2);
+        simulation.setAlive(3, 2);
+        simulation.setAlive(4, 2);
 
+        simulation.printBoard();
+
+        simulation.step();
+
+        simulation.printBoard();
+
+        simulation.step();
+
+        simulation.printBoard();
 
     }
 
